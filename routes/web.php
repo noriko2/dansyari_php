277f61c04f6d6aa64f5c96b4c6d0a56736b03e2c
noriko <nontaan55@yahoo.co.jp>
2021-11-01 15:01:46 +0900

    JSのパッケージをインストール($ php artisan jetstream:install livewire)

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [HomeController::class, 'index']);
 Route::get('/question', [HomeController::class, 'question'])->name('question');;
+
+Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
+    return view('dashboard');
+})->name('dashboard');
