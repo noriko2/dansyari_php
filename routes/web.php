3d5b5222d5ce915278a53d09e36d6110c21158b4
noriko <nontaan55@yahoo.co.jp>
2021-11-01 15:11:35 +0900

    jetstreamのwelcome.blade.phpが消えていたので追加

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,14 @@
 <?php
 
-use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
 
-Route::get('/', [HomeController::class, 'index']);
-Route::get('/question', [HomeController::class, 'question'])->name('question');;
+Route::get('/', function () {
+    return view('welcome');
+});
+
+// Route::get('/', [HomeController::class, 'index']);
+// Route::get('/question', [HomeController::class, 'question'])->name('question');;
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
